5dfd00e5ba01ce4a9a94320888b2099448e6bcf5
Minseung Kwon <bananapizzza@gmail.com>
2019-08-28 20:17:02 -0700

    - TCPServerProtocol.java / TCPClientProtocol.java: Javadoc added.

--- src/TCPServerProtocol.java
+++ src/TCPServerProtocol.java
@@ -3,10 +3,17 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * TCPServerProtocol is for a connection using Socket and ServerSocket.
+ * It provides methods for sending a packet and receiving a packet as server side.
+ */
 public class TCPServerProtocol extends TCPProtocol {
     private Socket clientSocket;
     private ServerSocket serverSocket;
 
+    /**
+     * Server side gets a port number as a param for constructor.
+     */
     public TCPServerProtocol(int port) {
         try {
             serverSocket = new ServerSocket(port);
@@ -15,6 +22,12 @@ public class TCPServerProtocol extends TCPProtocol {
         }
     }
 
+    /**
+     * Send a packet using TCP protocol.
+     * Since TCP connects with a client first and send packet, it doesn't need to get hostname or IP address again.
+     * Therefore we only use sendPacket(String msg) method.
+     * @param msg the message to send
+     */
     @Override
     public void sendPacket(String msg) {
         out.println(msg);
@@ -30,6 +43,11 @@ public class TCPServerProtocol extends TCPProtocol {
         sendPacket(msg);
     }
 
+    /**
+     * Receive a packet using TCP protocol.
+     *
+     * @return the data of the received packet
+     */
     @Override
     public String receivePacket() {
         if (clientSocket == null) {
@@ -50,6 +68,9 @@ public class TCPServerProtocol extends TCPProtocol {
         return receivedData;
     }
 
+    /**
+     * Close TCP socket and input/output stream.
+     */
     @Override
     public void closeSocket() {
         try {
